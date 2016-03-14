07db2cd497889c9e0bf3f0641f17c7b72291a421
Andres Felipe Leon Cruz <afleonc@gmail.com>
2016-03-14 01:09:56 -0500

    Creado administrador body y retocado interfaces

--- web/js/diagnosTyCs.js
+++ web/js/diagnosTyCs.js
@@ -3,6 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+
+function resizeIframe(obj) {
+    obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
+}
 function builder(){
     $('.selectpicker').selectpicker();
     $('.selectpicker').selectpicker('refresh');
