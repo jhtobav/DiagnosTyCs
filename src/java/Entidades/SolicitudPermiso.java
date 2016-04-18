13097b3e3a405fe9623e1b7d91f88986b93ed9e3
jhtob <jhtob@192.168.1.16>
2016-04-17 20:53:53 -0500

    Arreglo entidad generation type entity Solicitud

--- src/java/Entidades/SolicitudPermiso.java
+++ src/java/Entidades/SolicitudPermiso.java
@@ -10,6 +10,8 @@ import java.util.Date;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
@@ -41,8 +43,8 @@ public class SolicitudPermiso implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @Basic(optional = false)
-    @NotNull
     @Column(name = "solicitudPermisoID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long solicitudPermisoID;
     @Basic(optional = false)
     @NotNull
